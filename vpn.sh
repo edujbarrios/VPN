aace11bb07b8cb82baf3283d78f414644edc7497
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-01-19 10:55:19 +0000

    New features added

--- vpn.sh
+++ vpn.sh
@@ -24,6 +24,155 @@ list_servers() {
     # Insert command to list available VPN servers here
 }
 
+change_server() {
+    echo "Select a VPN server:"
+    # Insert command to list available VPN servers here
+    read -p "Enter the number of the server you want to connect to: " server_number
+    # Insert command to connect to the selected VPN server here
+}
+
+view_log() {
+    echo "VPN connection log:"
+    # Insert command to view the VPN connection log here
+}
+
+set_credentials() {
+    read -p "Enter your VPN username: " vpn_username
+    read -p "Enter your VPN password: " vpn_password
+    # Insert command to set the credentials in your VPN config file
+}
+
+change_protocol() {
+    echo "Select a protocol:"
+    echo "1. OpenVPN"
+    echo "2. PPTP"
+    echo "3. L2TP"
+    read -p "Enter the number of the protocol you want to use: " protocol_number
+    case $protocol_number in
+        1) protocol="openvpn";;
+        2) protocol="pptp";;
+        3) protocol="l2tp";;
+        *) echo "Invalid choice. Please enter a number between 1 and 3.";;
+    esac
+    # Insert command to change the protocol in your VPN config file
+}
+
+auto_connect() {
+    read -p "Do you want to enable auto-connect? (y/n) " choice
+    case $choice in
+        y|Y) auto_connect=true;;
+        n|N) auto_connect=false;;
+        *) echo "Invalid choice. Please enter y or n.";;
+    esac
+    # Insert command to set auto-connect in your VPN config file
+}
+
+save_config() {
+    read -p "Enter a name for the configuration: " config_name
+    # Insert command to save the configuration to a file
+}
+
+import_config() {
+    echo "List of available configurations:"
+    # Insert command to list available config files
+    read -p "Enter the name of the configuration you want to import: " config_name
+    # Insert command to import the selected config file
+}
+custom_dns() {
+    read -p "Enter your preferred primary DNS server: " primary_dns
+    read -p "Enter your preferred secondary DNS server: " secondary_dns
+    # Insert command to set custom DNS servers
+}
+# This function allows the user to enable/disable the kill switch feature, which will automatically disconnect the internet if the VPN connection drops.
+kill_switch() {
+    read -p "Do you want to enable the kill switch? (y/n) " choice
+    case $choice in
+        y|Y) kill_switch=true;;
+        n|N) kill_switch=false;;
+        *) echo "Invalid choice. Please enter y or n.";;
+    esac
+    # Insert command to set kill switch feature
+}
+
+change_credentials() {
+    read -p "Enter new VPN username: " vpn_username
+    read -p "Enter new VPN password: " vpn_password
+    # Insert command to change the login credentials
+}
+
+#  This function allows the user to enable/disable split tunneling, which routes only specific traffic through the VPN while allowing other traffic to go through the regular internet connection.
+split_tunneling() {
+    read -p "Do you want to enable split tunneling? (y/n) " choice
+    case $choice in
+        y|Y) split_tunneling=true;;
+        n|N) split_tunneling=false;;
+        *) echo "Invalid choice. Please enter y or n.";;
+    esac
+    # Insert command to set split tunneling feature
+}
+
+change_port() {
+    read -p "Enter the desired port number: " port_number
+    # Insert command to change the port number in the VPN config file
+}
+
+change_encryption() {
+    echo "Select an encryption algorithm:"
+    echo "1. AES-256"
+    echo "2. AES-128"
+    echo "3. Blowfish"
+    read -p "Enter the number of the encryption algorithm: " encryption_number
+    case $encryption_number in
+        1) encryption="AES-256";;
+        2) encryption="AES-128";;
+        3) encryption="Blowfish";;
+        *) echo "Invalid choice. Please enter a number between 1 and 3.";;
+    esac
+    # Insert command to change the encryption algorithm in the VPN config file
+}
+change_cipher() {
+    echo "Select a cipher:"
+    echo "1. AES-256-CBC"
+    echo "2. AES-128-CBC"
+    echo "3. Blowfish-CBC"
+    read -p "Enter the number of the cipher: " cipher_number
+    case $cipher_number in
+        1) cipher="AES-256-CBC";;
+        2) cipher="AES-128-CBC";;
+        3) cipher="Blowfish-CBC";;
+        *) echo "Invalid choice. Please enter a number between 1 and 3.";;
+    esac
+    # Insert command to change the cipher in the VPN config file
+}
+
+block_ads() {
+    read -p "Do you want to block ads? (y/n) " choice
+    case $choice in
+        y|Y) block_ads=true;;
+        n|N) block_ads=false;;
+        *) echo "Invalid choice. Please enter y or n.";;
+    esac
+    # Insert command to block ads on the VPN connection
+}
+
+auto_reconnect() {
+    read -p "Do you want to enable auto-reconnect? (y/n) " choice
+    case $choice in
+        y|Y) auto_reconnect=true;;
+        n|N) auto_reconnect=false;;
+        *) echo "Invalid choice. Please enter y or n.";;
+    esac
+    # Insert command to enable auto-reconnect in the VPN config file
+}
+
+speed_test() {
+    # sudo pip install speedtest-cli
+    speedtest-cli
+    
+}
+
+
+
 # Menu loop
 while true; do
     echo "VPN Menu"
@@ -31,7 +180,24 @@ while true; do
     echo "2. Disconnect"
     echo "3. Check Status"
     echo "4. List Servers"
-    echo "5. Exit"
+    echo "5. Change Server"
+    echo "6. View Log"
+    echo "7. Set Credentials"
+    echo "8. Change Protocol"
+    echo "9. Auto Connect"
+    echo "10. Save Configuration"
+    echo "11. Import Configuration"
+    echo "12. Set Custom DNS"
+    echo "13. Kill Switch"
+    echo "14. Change Credentials"
+    echo "15. Split Tunneling"
+    echo "16. Change Port"
+    echo "17. Change Encryption"
+    echo "18. Change Cipher"
+    echo "19. Ad Block"
+    echo "20. Auto Reconnect"
+    echo "21. Speed Test"
+    echo "99. Exit"
     read -p "Enter your choice: " choice
 
     case $choice in
@@ -39,7 +205,25 @@ while true; do
         2) disconnect_vpn;;
         3) check_status;;
         4) list_servers;;
-        5) exit;;
+        5) change_servers;;
+        6) view_log;;
+        7) set_credentials;;
+        8) change_protocol;;
+        9) auto_connect;;
+        10) save_config;;
+        11) import_config;;
+        12) custom_dns;;
+        13) kill_switch;;
+        14) change_credentials;;
+        15) split_tunneling;;
+        16) change_port;;
+        17) change_encryption;;
+        18( change_cipher;;
+        19) block_ads;;
+        20) auto_reconnect;;
+        21) speed_test;;
+        99) exit;;
+        
         *) echo "Invalid choice. Please enter a number between 1 and 5.";;
     esac
 done
